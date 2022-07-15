00588fe59b2857c459ce2983804963da682c3327
abheist <abhishek291093@gmail.com>
2022-07-15 00:13:30 -0300

    intermediate

--- even_odd.py
+++ even_odd.py
@@ -1,16 +1,17 @@
 from typing import List
 
 
-def even_odd(A: List[int]) -> List[int]:
-    next_even, next_odd = 0, len(A) - 1
+def even_odd(a: List[int]) -> List[int]:
+    next_even, next_odd = 0, len(a) - 1
     while next_even < next_odd:
         print('running')
-        if A[next_even]  % 2 == 0:
+        if a[next_even] % 2 == 0:
             next_even += 1
         else:
-            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            a[next_even], a[next_odd] = a[next_odd], a[next_even]
             next_odd -= 1
-    return A
+    return a
 
-print(even_odd([1,2,2,1]))
+
+print(even_odd([1, 2, 2, 1]))
 
